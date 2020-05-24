1edbcb452e62070e26950406edc1976c681e22c4
shishishi1320 <shishishi.1320@gmail.com>
2020-05-24 17:19:22 +0900

    a

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,2 +1,37 @@
 class HealthsController < ApplicationController
+  def index
+    @healths = Health.all
+  end 
+
+  def new
+    @health = Health.new
+  end
+
+  def create
+    Health.create(health_params)
+    redirect_to healths_path
+  end
+
+  def show
+    @health = Health.find(params[:id])
+  end
+  def edit
+    @health = Health.find(params[:id]) 
+  end
+
+  def update
+    health = Health.find(params[:id])
+    health.update(health_params)
+    redirect_to root_path
+  end
+  
+  def destroy
+    health = Health.find(params[:id])
+    health.destroy
+    redirect_to root_path
+  end
+  private
+  def health_params
+    params.require(:health).permit(:title, :content)
+  end
 end
